865e518a9e539c665e9c86f99d8ecbf48f47040a
Mao <maoacr@gmail.com>
2024-10-04 22:11:31 -0500

    [FIX] relocate image to fix vite error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   adapter: netlify(),
   site: "https://wearesorcol.com",
   integrations: [
