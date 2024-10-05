d4ffceaa243de710664a009e50c440ba66083c0b
Mao <maoacr@gmail.com>
2024-10-04 22:16:05 -0500

    Deleting Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
   site: "https://wearesorcol.com",
   integrations: [
     starlight({
