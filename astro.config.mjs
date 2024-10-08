4b5cb55dfd54882ef235013ea3b5b1552146bdb0
Mao <maoacr@gmail.com>
2024-10-08 00:12:26 -0500

    [FIX] Change images paths to public

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,14 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        "@assets": "/src/assets",
-      },
-    },
-  },
   site: "https://wearesorcol.com",
   integrations: [
     starlight({
-      favicon: "./src/assets/hand-base.png",
+      favicon: "/public/hand-base.png",
       defaultLocale: "es",
       title: "Documentación de Sorcol",
       logo: {
-        src: "./src/assets/hand-base.png",
+        src: "/public/hand-base.png",
         alt: "Sorcol's brand",
       },
       social: {
